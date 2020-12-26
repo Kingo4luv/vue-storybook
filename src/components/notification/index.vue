eac838585354fe4f118be0e0dd9be733e34b5a98
Kingsley Atanang <Kingo4luv@yahoo.com>
2020-12-27 00:08:58 +0100

    added types to notification

--- src/components/notification/index.vue
+++ src/components/notification/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="w-full h-screen flex justify-center items-center">
         <div>
-            <button class="px-6 py-2 bg-blue-700 text-white rounded" @click="open">Show</button>
+            <button class="px-6 py-2 bg-blue-700 text-white rounded" @click="open">Open Notification</button>
         </div>
         <div class="notification">
             <transition
@@ -12,14 +12,19 @@
             :leave-from-class="transitionLeaveFrom"
             :leave-to-class="transitionLeaveTo"
             >
-                <div :class="direction" class="fixed w-1/4 bg-white shadow-lg space-y-2 p-4 border border-gray-100" v-show="show">
+                <div :class="direction" class="fixed w-full sm:w-1/4 bg-white shadow-lg space-y-2 p-4 border border-gray-100 z-10" v-show="show">
                     <div class="flex justify-between items-center">
-                        <h3 class="font-medium text-gray-700">Notification Title</h3>
+                        <div class="flex items-center">
+                            <span v-show="showIcon" class="mr-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6" :class="color" v-html="icon"></svg>
+                            </span>
+                            <h3 class="font-medium text-gray-700">Notification Title</h3>
+                        </div>
                         <button class="cursor-pointer" @click="dismiss">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6" :class="color"><path d="M16.24 14.83a1 1 0 0 1-1.41 1.41L12 13.41l-2.83 2.83a1 1 0 0 1-1.41-1.41L10.59 12 7.76 9.17a1 1 0 0 1 1.41-1.41L12 10.59l2.83-2.83a1 1 0 0 1 1.41 1.41L13.41 12l2.83 2.83z"/></svg>
                         </button>
                     </div>
-                    <p class="text-sm tracking-wide leading-5 text-gray-600">
+                    <p class="text-sm leading-6 text-gray-600">
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit placeat a, dolor soluta labore nesciunt, error, tempore harum mollitia cupiditate delectus sapiente. Nostrum totam similique modi libero praesentium asperiores neque.
                     </p>
                 </div>
@@ -30,9 +35,17 @@
 <script>
 export default {
     props: {
+        type: {
+            type: String,
+            required: true,
+        },
         position: {
             type: String,
-            default: "bottom-left",
+            default: "top-right",
+        },
+        visible: {
+            type: Boolean,
+            required: true
         },
         showIcon:{
             type: Boolean,
@@ -49,7 +62,7 @@ export default {
     },
     data(){
         return{
-            show: false
+            show: this.visible
         }
     },
     computed:{
@@ -86,6 +99,32 @@ export default {
                 return `-translate-x-full`;
             }
             return `translate-x-full`;
+        },
+        color(){
+            if(this.type === "primary"){
+                return "text-blue-600";
+            }else if(this.type === "danger") {
+                return "text-red-600";
+            }else if(this.type === "warning") {
+                return "text-yellow-500";
+            }
+            else if(this.type === "success") {
+                return "text-green-600";
+            }
+            return "text-gray-700";
+        },
+        icon(){
+            if(this.type === "primary"){
+                return  `<path d="M12 2a10 10 0 1 1 0 20 10 10 0 0 1 0-20zm0 2a8 8 0 1 0 0 16 8 8 0 0 0 0-16zm0 9a1 1 0 0 1-1-1V8a1 1 0 0 1 2 0v4a1 1 0 0 1-1 1zm0 4a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/>`;
+            }else if(this.type === "danger") {
+                return `<path d="M4.93 19.07A10 10 0 1 1 19.07 4.93 10 10 0 0 1 4.93 19.07zm1.41-1.41A8 8 0 1 0 17.66 6.34 8 8 0 0 0 6.34 17.66zM13.41 12l1.42 1.41a1 1 0 1 1-1.42 1.42L12 13.4l-1.41 1.42a1 1 0 1 1-1.42-1.42L10.6 12l-1.42-1.41a1 1 0 1 1 1.42-1.42L12 10.6l1.41-1.42a1 1 0 1 1 1.42 1.42L13.4 12z"/>`;
+            }else if(this.type === "warning") {
+                return `<path d="M12 2a10 10 0 1 1 0 20 10 10 0 0 1 0-20zm0 2a8 8 0 1 0 0 16 8 8 0 0 0 0-16zm0 9a1 1 0 0 1-1-1V8a1 1 0 0 1 2 0v4a1 1 0 0 1-1 1zm0 4a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/>`;
+            }
+            else if(this.type === "success") {
+                return `<path d="M12 22a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm-2.3-8.7l1.3 1.29 3.3-3.3a1 1 0 0 1 1.4 1.42l-4 4a1 1 0 0 1-1.4 0l-2-2a1 1 0 0 1 1.4-1.42z"/>`;
+            }
+            return `<path d="M12 2a10 10 0 1 1 0 20 10 10 0 0 1 0-20zm0 2a8 8 0 1 0 0 16 8 8 0 0 0 0-16zm0 9a1 1 0 0 1-1-1V8a1 1 0 0 1 2 0v4a1 1 0 0 1-1 1zm0 4a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/>`;
         }
     },
     methods:{
--- src/stories/Notification.stories.js
+++ src/stories/Notification.stories.js
@@ -14,12 +14,84 @@ const Template = (args, { argTypes }) => ({
 export const Default = Template.bind({});
 Default.args = {
   title: "Notification Title" ,
-  content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores nulla mollitia laudantium reiciendis, quis non consectetur ipsum? Porro nesciunt consequatur provident qui accusantium, cum animi quam quibusdam odit voluptatibus! Reiciendis."
+  content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores nulla mollitia laudantium reiciendis, quis non consectetur ipsum? Porro nesciunt consequatur provident qui accusantium, cum animi quam quibusdam odit voluptatibus! Reiciendis.",
+  visible: true  
 };
 
-// export const Open = Template.bind({});
-// Open.args = {
-//   title: "Open Accordion" ,
-//   content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores nulla mollitia laudantium reiciendis, quis non consectetur ipsum? Porro nesciunt consequatur provident qui accusantium, cum animi quam quibusdam odit voluptatibus! Reiciendis.",
-//   showContent: true
-// };
+export const Closed  = Template.bind({});
+Closed.args = {
+  title: "Notification Title" ,
+  content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores nulla mollitia laudantium reiciendis, quis non consectetur ipsum? Porro nesciunt consequatur provident qui accusantium, cum animi quam quibusdam odit voluptatibus! Reiciendis.",
+  visible: false
+};
+
+export const TopRight  = Template.bind({});
+TopRight.args = {
+  title: "Notification Title" ,
+  content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores nulla mollitia laudantium reiciendis, quis non consectetur ipsum? Porro nesciunt consequatur provident qui accusantium, cum animi quam quibusdam odit voluptatibus! Reiciendis.",
+  visible: false,
+  position: "top-right"
+};
+export const TopLeft  = Template.bind({});
+TopLeft.args = {
+  title: "Notification Title" ,
+  content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores nulla mollitia laudantium reiciendis, quis non consectetur ipsum? Porro nesciunt consequatur provident qui accusantium, cum animi quam quibusdam odit voluptatibus! Reiciendis.",
+  visible: false,
+  position: "top-left"
+};
+export const BottomLeft  = Template.bind({});
+BottomLeft.args = {
+  title: "Notification Title" ,
+  content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores nulla mollitia laudantium reiciendis, quis non consectetur ipsum? Porro nesciunt consequatur provident qui accusantium, cum animi quam quibusdam odit voluptatibus! Reiciendis.",
+  visible: false,
+  position: "bottom-left"
+};
+
+export const BottomRight  = Template.bind({});
+BottomRight.args = {
+  title: "Notification Title" ,
+  content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores nulla mollitia laudantium reiciendis, quis non consectetur ipsum? Porro nesciunt consequatur provident qui accusantium, cum animi quam quibusdam odit voluptatibus! Reiciendis.",
+  visible: false,
+  position: "bottom-right"
+};
+
+export const WithIcon  = Template.bind({});
+WithIcon.args = {
+  title: "Notification Title" ,
+  content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores nulla mollitia laudantium reiciendis, quis non consectetur ipsum? Porro nesciunt consequatur provident qui accusantium, cum animi quam quibusdam odit voluptatibus! Reiciendis.",
+  visible: false,
+  showIcon: true
+};
+
+export const Success  = Template.bind({});
+Success.args = {
+  title: "Notification Title" ,
+  content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores nulla mollitia laudantium reiciendis, quis non consectetur ipsum? Porro nesciunt consequatur provident qui accusantium, cum animi quam quibusdam odit voluptatibus! Reiciendis.",
+  visible: false,
+  showIcon: true,
+  type: "success"
+};
+export const Primary  = Template.bind({});
+Primary.args = {
+  title: "Notification Title" ,
+  content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores nulla mollitia laudantium reiciendis, quis non consectetur ipsum? Porro nesciunt consequatur provident qui accusantium, cum animi quam quibusdam odit voluptatibus! Reiciendis.",
+  visible: false,
+  showIcon: true,
+  type: "primary"
+};
+export const Warning  = Template.bind({});
+Warning.args = {
+  title: "Notification Title" ,
+  content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores nulla mollitia laudantium reiciendis, quis non consectetur ipsum? Porro nesciunt consequatur provident qui accusantium, cum animi quam quibusdam odit voluptatibus! Reiciendis.",
+  visible: false,
+  showIcon: true,
+  type: "warning"
+};
+export const Danger  = Template.bind({});
+Danger.args = {
+  title: "Notification Title" ,
+  content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores nulla mollitia laudantium reiciendis, quis non consectetur ipsum? Porro nesciunt consequatur provident qui accusantium, cum animi quam quibusdam odit voluptatibus! Reiciendis.",
+  visible: false,
+  showIcon: true,
+  type: "danger"
+};
