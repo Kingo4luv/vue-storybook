1de81cb85c9fa7e1924e2769d0caf3b4f251ec22
Kingsley Atanang <Kingo4luv@yahoo.com>
2021-01-19 22:58:18 +0100

    small pading in search

--- src/components/select/Select.vue
+++ src/components/select/Select.vue
@@ -32,7 +32,7 @@
                     -->
                     <div v-show="open" class="absolute mt-1 w-full rounded bg-white shadow-xl border border-gray-100">
                         <div class="flex items-center border-b">
-                            <span class="px-2">
+                            <span class="pl-4 pr-2">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 mt-1 text-gray-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
                                 </svg>
@@ -90,8 +90,8 @@ export default {
         return{
             open: false,
             currentCountry: {
-                name: "USA",
-                icon: 'us'
+                name: "Nigeria",
+                icon:"ng"
             },
             search: "",
             data: [
@@ -118,6 +118,14 @@ export default {
                {
                     name: "China",
                     icon:"cn"
+               },
+               {
+                    name: "Ghana",
+                    icon:"gh"
+               },
+               {
+                    name: "Nigeria",
+                    icon:"ng"
                },
                {
                     name: "USA",
