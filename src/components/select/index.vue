7313072713d35857d192ad2dcb055bea25fe47f2
Kingsley Atanang <Kingo4luv@yahoo.com>
2021-01-22 20:53:33 +0100

    refactored select

---
+++ src/components/select/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="w-full h-screen bg-gray-100 flex">
+           <div class="mx-auto mt-12 w-full sm:w-1/4">
+                <Select />
+           </div>
+    </div>
+</template>
+
+<script>
+import Select from './Select'
+export default {
+    components:{
+        Select
+    }
+}
+</script>
--- src/stories/Select.stories.js
+++ src/stories/Select.stories.js
@@ -1,17 +1,21 @@
-import Select from '../components/select/Select.vue'
+import Select from '../components/select/index.vue'
 
 export default {
   title: 'Components/Select',
   component: Select,
 };
 
-const Template = (args, { argTypes }) => ({
+const Template = (args, {
+  argTypes
+}) => ({
   props: Object.keys(argTypes),
-  components: { Select },
+  components: {
+    Select
+  },
   template: '<Select @onClick="onClick" v-bind="$props" />',
 });
 
-export const Default  = Template.bind({});
+export const Default = Template.bind({});
 Default.args = {
   visible: false,
 };
