76f01b9fef58a2606d5637590b1c2d514f853fee
Kingsley Atanang <Kingo4luv@yahoo.com>
2021-02-03 20:58:08 +0100

    started card component

---
+++ src/components/card/container.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="w-full flex">
+           <div class="mx-auto mt-12 w-full h-52 lg:h-96 sm:w-2/5">
+               <Card />
+           </div>
+    </div>
+</template>
+<script>
+import Card from './Card'
+export default {
+    props:{
+        
+    },
+    components:{
+        Card
+    }
+}
+</script>
