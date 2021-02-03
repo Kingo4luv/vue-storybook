c52ecb58eed7b0cbada8eb54ed8223e39e25ff26
Kingsley Atanang <Kingo4luv@yahoo.com>
2021-02-03 21:15:26 +0100

    added image to card

--- src/components/card/Card.vue
+++ src/components/card/Card.vue
@@ -1,12 +1,15 @@
 <template>
     <div>
-       <div class="w-full bg-white p-4 rounded">
-           <h2 class="card-title text-gray-700 font-semibold text-xl">
-               Card Title
-           </h2>
-           <p class="text-gray-600 leading-6">
-               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam, deleniti repellendus, fugiat debitis necessitatibus odit, inventore facere nam labore non consequatur eos aliquam blanditiis iure! Dolor assumenda dolore placeat inventore!
-           </p>
+       <div class="w-full bg-white rounded">
+           <img src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" alt="card-img" class="w-full h-52 rounded-t object-cover">
+           <div class="card-body mt-2 p-4">
+               <h2 class="card-title text-gray-700 font-semibold text-xl">
+                    Card Title
+                </h2>
+                <p class="text-gray-600 leading-6">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam, deleniti repellendus, fugiat debitis necessitatibus odit, inventore facere nam labore non consequatur eos aliquam blanditiis iure! Dolor assumenda dolore placeat inventore!
+                </p>
+           </div>
        </div>
     </div>
 </template>
