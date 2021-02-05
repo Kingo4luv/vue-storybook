44f097a76132e02753df92539c138b3f4bcd5995
Kingsley Atanang <Kingo4luv@yahoo.com>
2021-02-05 22:25:19 +0100

    started list

---
+++ src/components/lists/container.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="w-full flex bg-gray-100">
+           <div class="mx-auto mt-12 w-full h-screen sm:max-w-sm">
+               <List />
+           </div>
+    </div>
+</template>
+<script>
+import List from './List'
+export default {
+    // props:{
+    //     withImage: {
+    //         type: Boolean,
+    //         default: false
+    //     },
+    //     withAction:{
+    //         type: Boolean,
+    //         default: false
+    //     },
+    //     title:{
+    //         type: String,
+    //         required: true
+    //     }
+    // },
+    components:{
+        List
+    }
+}
+</script>
